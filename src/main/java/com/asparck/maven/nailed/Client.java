cc6317218828dec94b618221ecdd18849504fb9e
Caspar Krieger <ckrieger@atlassian.com>
2014-04-04 20:16:51 +1100

    Alternate work around for system property setting
    Copy, paste, and modify the MavenCli class to avoid setting system
    properties. This might break system property profile activation
    (although I was unable to cause it to break in my tests), but it does
    make the server safe for use by multiple builds again (as far as I
    know).

--- src/main/java/com/asparck/maven/nailed/Client.java
+++ src/main/java/com/asparck/maven/nailed/Client.java
@@ -1,12 +1,8 @@
 package com.asparck.maven.nailed;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
 import com.martiansoftware.nailgun.NGContext;
 
-import org.apache.maven.cli.MavenCli;
+import org.apache.maven.cli.NoSyspropChangingMavenCli;
 
 public class Client {
 
@@ -15,26 +11,7 @@ public class Client {
     }
 
     private static int run(NGContext context) throws Exception {
-        HashMap<Object, Object> originalSystemProperties = new HashMap<Object, Object>(System.getProperties());
-        try {
-            MavenCli cli = new MavenCli(Server.currentClassWorld);
-            // nailgun has already fixed System.out - pass null so that maven won't try to re-fix it
-            return cli.doMain(context.getArgs(), context.getWorkingDirectory(), null, null);
-        } finally {
-            // maven cli sets -D properties as system properties, which will interfere with our next invocation if we
-            // don't unset them again
-            restoreOriginalSystemProperties(originalSystemProperties);
-        }
-    }
-
-    private static void restoreOriginalSystemProperties(HashMap<Object, Object> originalSystemProperties) {
-        Set<String> currentProperties = System.getProperties().stringPropertyNames();
-        for (String currentProperty : currentProperties) {
-            System.clearProperty(currentProperty);
-        }
-        for (Map.Entry<Object, Object> originalPropertyEntry : originalSystemProperties.entrySet())
-        {
-            System.setProperty(originalPropertyEntry.getKey().toString(), originalPropertyEntry.getValue().toString());
-        }
+        NoSyspropChangingMavenCli cli = new NoSyspropChangingMavenCli(Server.currentClassWorld);
+        return cli.doMain(context.getArgs(), context.getWorkingDirectory(), null, null);
     }
 }
