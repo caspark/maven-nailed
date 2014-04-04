0369fc055243fa54002c74b197add18d35591e2a
Caspar Krieger <ckrieger@atlassian.com>
2014-04-04 20:06:11 +1100

    Use classworld provided to Server

--- src/main/java/com/asparck/maven/nailed/Client.java
+++ src/main/java/com/asparck/maven/nailed/Client.java
@@ -1,7 +1,8 @@
 package com.asparck.maven.nailed;
 
-import org.apache.maven.cli.MavenCli;
 import com.martiansoftware.nailgun.NGContext;
+
+import org.apache.maven.cli.MavenCli;
 
 public class Client {
 
@@ -10,9 +11,8 @@ public class Client {
     }
 
     private static int run(NGContext context) throws Exception {
-        MavenCli cli = new MavenCli();
+        MavenCli cli = new MavenCli(Server.currentClassWorld);
         // nailgun has already fixed System.out - pass null so that maven won't try to re-fix it
         return cli.doMain(context.getArgs(), context.getWorkingDirectory(), null, null);
     }
-
 }
--- src/main/java/com/asparck/maven/nailed/Server.java
+++ src/main/java/com/asparck/maven/nailed/Server.java
@@ -2,15 +2,20 @@ package com.asparck.maven.nailed;
 
 import java.net.InetAddress;
 
+import org.codehaus.plexus.classworlds.ClassWorld;
+
 import com.martiansoftware.nailgun.NGServer;
 
 public class Server {
 
-    public static void main(String[] args) throws Exception {
+    public static volatile ClassWorld currentClassWorld;
+
+    public static void main(String[] args, ClassWorld classWorld) throws Exception {
         if (args.length != 1) {
             System.err.println("Expecting exactly one arg: [host:]port");
             return;
         }
+        Server.currentClassWorld = classWorld;
 
         String[] argParts = args[0].split(":");
         InetAddress serverAddress = InetAddress.getByName(argParts[0]);
