93284f8b000e9d4d9ba6dca03f42efdfd1715459
Caspar Krieger <ckrieger@atlassian.com>
2014-04-04 20:08:58 +1100

    Restore system properties after they've been changed by Maven CLI
    Maven changes system properties which causes them to stick around for
    the next Maven invocation. This work around restores the system property
    state back to what it was before Maven was involved after each time that
    Maven gets involved.

--- src/main/java/com/asparck/maven/nailed/Client.java
+++ src/main/java/com/asparck/maven/nailed/Client.java
@@ -1,5 +1,9 @@
 package com.asparck.maven.nailed;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 import com.martiansoftware.nailgun.NGContext;
 
 import org.apache.maven.cli.MavenCli;
@@ -11,8 +15,26 @@ public class Client {
     }
 
     private static int run(NGContext context) throws Exception {
-        MavenCli cli = new MavenCli(Server.currentClassWorld);
-        // nailgun has already fixed System.out - pass null so that maven won't try to re-fix it
-        return cli.doMain(context.getArgs(), context.getWorkingDirectory(), null, null);
+        HashMap<Object, Object> originalSystemProperties = new HashMap<Object, Object>(System.getProperties());
+        try {
+            MavenCli cli = new MavenCli(Server.currentClassWorld);
+            // nailgun has already fixed System.out - pass null so that maven won't try to re-fix it
+            return cli.doMain(context.getArgs(), context.getWorkingDirectory(), null, null);
+        } finally {
+            // maven cli sets -D properties as system properties, which will interfere with our next invocation if we
+            // don't unset them again
+            restoreOriginalSystemProperties(originalSystemProperties);
+        }
+    }
+
+    private static void restoreOriginalSystemProperties(HashMap<Object, Object> originalSystemProperties) {
+        Set<String> currentProperties = System.getProperties().stringPropertyNames();
+        for (String currentProperty : currentProperties) {
+            System.clearProperty(currentProperty);
+        }
+        for (Map.Entry<Object, Object> originalPropertyEntry : originalSystemProperties.entrySet())
+        {
+            System.setProperty(originalPropertyEntry.getKey().toString(), originalPropertyEntry.getValue().toString());
+        }
     }
 }
