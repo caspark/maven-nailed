a26a6736bff32bd9ce738cf451970e5715b0a762
Caspar Krieger <ckrieger@atlassian.com>
2014-04-04 17:12:54 +1100

    Don't bother forking
    Still seems to work without it.

--- src/main/java/com/asparck/maven/nailed/Server.java
+++ src/main/java/com/asparck/maven/nailed/Server.java
@@ -2,15 +2,11 @@ package com.asparck.maven.nailed;
 
 import java.net.InetAddress;
 
-import org.codehaus.plexus.classworlds.ClassWorld;
-
-import com.martiansoftware.nailgun.NGConstants;
 import com.martiansoftware.nailgun.NGServer;
 
 public class Server {
 
-    public static void main(String[] args, ClassWorld classWorld) throws Exception {
-        // bah, this should just be a call to NGServer.main(), but plexus doesn't want us to just return
+    public static void main(String[] args) throws Exception {
         if (args.length != 1) {
             System.err.println("Expecting exactly one arg: [host:]port");
             return;
@@ -21,24 +17,7 @@ public class Server {
         int port = Integer.parseInt(argParts[1]);
 
         NGServer server = new NGServer(serverAddress, port, NGServer.DEFAULT_SESSIONPOOLSIZE);
-        Thread t = new Thread(server);
-        t.setName("NGServer(" + serverAddress + ", " + port + ")");
-        t.start();
-
-        int runningPort = server.getPort();
-        while (runningPort == 0) {
-            Thread.sleep(50);
-            runningPort = server.getPort();
-        }
-
-        System.out.println("NGServer "
-                + NGConstants.VERSION
-                + " started on "
-                + ((serverAddress == null) ? "all interfaces" : serverAddress.getHostAddress())
-                + ", port " + runningPort + ".");
-
-        // wait until someone tells us to die
-        t.join();
+        server.run();
     }
 
 }
